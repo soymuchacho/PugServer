9f0d0fb63a1855b395beb01b8b2f80473b7f5cc1
soymuchacho <guqi_282@126.com>
2021-02-07 06:24:31 +0000

    add main

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"./service"
+	"./util"
+	log "github.com/cihub/seelog"
+	"time"
+)
+
+func InitLog() {
+	logger, err := log.LoggerFromConfigAsFile("conf/seelog.xml")
+	if err != nil {
+		panic(err)
+	}
+
+	log.ReplaceLogger(logger)
+	log.Debug("############################################################################")
+	log.Debug("#						      PugServer									  #")
+	log.Debug("############################################################################")
+}
+
+func InitConfig() {
+
+}
+
+func main() {
+	defer log.Flush()
+
+	InitLog()
+	InitConfig()
+
+	addr, err := util.Cfg.GetValue("", "ServerAddr")
+	if err != nil {
+		log.Error("can't find addr:", err)
+		panic(err)
+	}
+
+	go service.Run(addr)
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
